089f0759011303973472d4b9c2553cf66d143db7
Revan707 <ravangh@code.edu.az>
2023-06-01 22:41:20 +0400

    Page's dropdown is done

--- Assets/Js/blog.js
+++ Assets/Js/blog.js
@@ -1,15 +1,28 @@
 const container =document.querySelector(".container")
 const section2=document.querySelector(".section-2")
 const home=document.querySelector(".home")
-const pages1=document.querySelector(".pages1")
+const pages=document.querySelector(".pages")
+const homeDropDown=document.querySelector(".home_dropdown")
+const pageDropDown=document.querySelector(".page_dropdown")
 const angle=document.querySelector(".angle")
+const angle2=document.querySelector(".angle2")
 
 state="flex"
 state2="rotate(180deg)"
 home.addEventListener("click",()=>{
-    pages1.style.display=state
+    pageDropDown.style.display="none"
+    homeDropDown.style.display=state
     state=state=="flex"? "none":"flex"
 
     angle.style.transform=state2
+    state2=state2=="rotate(180deg)"? "rotate(0deg)":"rotate(180deg)"
+})
+
+pages.addEventListener("click",()=>{
+    homeDropDown.style.display="none"
+    pageDropDown.style.display=state
+    state=state=="flex"? "none":"flex"
+
+    angle2.style.transform=state2
     state2=state2=="rotate(180deg)"? "rotate(0deg)":"rotate(180deg)"
 })
