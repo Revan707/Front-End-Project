6db8084c45b97415afd523474ceb753c505fcf43
Revan707 <ravangh@code.edu.az>
2023-06-01 19:55:34 +0400

    Home dropdown is done

--- Assets/Js/blog.js
+++ Assets/Js/blog.js
@@ -1,4 +1,15 @@
+const container =document.querySelector(".container")
 const section2=document.querySelector(".section-2")
-new simpleParallax(section2,{
-    scale:1.5
+const home=document.querySelector(".home")
+const pages1=document.querySelector(".pages1")
+const angle=document.querySelector(".angle")
+
+state="flex"
+state2="rotate(180deg)"
+home.addEventListener("click",()=>{
+    pages1.style.display=state
+    state=state=="flex"? "none":"flex"
+
+    angle.style.transform=state2
+    state2=state2=="rotate(180deg)"? "rotate(0deg)":"rotate(180deg)"
 })
